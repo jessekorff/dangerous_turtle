bedd1271574583702d06ee89a258fe4e1b5fb09b
jessekorff <jessekorff@gmail.com>
2025-04-15 12:21:27 -0400

    add follower count file

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0; //Initialize count to 0
+
+function increaseCount(){
+    count++; // Increase the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value & display msgs
+}
+
+function checkCountValue(){
+    if (count===10) {
+        alert("Your account gained 10 followers! Congratulations!");
+    } else if (count===20) {
+        alert("Your account gained 20 followers! Yay!");
+    }
+}
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count; //Display the count in HTML
+
+}
