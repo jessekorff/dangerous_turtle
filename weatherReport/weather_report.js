d0f5c756abb7399b2a7efbc24511044d9ee20f37
jessekorff <jessekorff@gmail.com>
2025-04-29 11:23:06 -0400

    added Weather Report files

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,22 @@
+function showWeatherDetails(event) {
+    event.preventDefault();
+
+    const city = document.getElementById('city').value;
+     const apiKey = 'b4c0f6ad1835fc4aba8e5c3db983d680';
+     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+     fetch(apiUrl)
+     .then(response => response.json())
+     .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                <p>Temerature: ${data.main.temp} &#8451;</p>
+                                <p>Weather: ${data.weather[0].description}</p>`;
+     })
+     .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+      });
+}
+document.getElementById('weatherForm').addEventListener('submit',showWeatherDetails );
